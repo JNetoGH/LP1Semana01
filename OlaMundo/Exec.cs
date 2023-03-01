3f82248173ef165074bf75b14b732dae131ff94d
Joao Neto <joaomendes15@gmail.com>
2023-03-01 23:55:23 +0000

    OlaMundo done

---
+++ OlaMundo/Exec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OlaMundo {
+    
+    /// <summary>
+    ///     This class is used simply to hold the Main method.
+    /// </summary>
+    internal static class Exec {
+        
+        /// <summary>
+        ///     It's the entrance of the program
+        /// </summary>
+        /// <param name="args">
+        ///     Can be set at the program execution
+        /// </param>
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}

4fb24711972499616a8983dde4aa55354cc30115
Joao Neto <joaomendes15@gmail.com>
2023-03-01 23:48:32 +0000

    Argumentos done

---
+++ Argumentos/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+
+namespace Argumentos {
+    internal static class Program {
+        static void Main(string[] args) => args.ToList().ForEach(s => Console.WriteLine(s));
+    }
+}

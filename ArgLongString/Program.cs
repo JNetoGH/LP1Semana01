dc8a55dd330e2e22f6908816affb6b85c2229414
João Neto <a22200558@alunos.ulht.pt>
2023-03-03 13:53:31 +0000

    ArgLongString added

---
+++ ArgLongString/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace ArgLongString{
+    class Exec {
+        static void Main(string[] args) {
+            args.ToList().ForEach(s => Console.WriteLine(s.Length > 4 ? s.ToUpper() : ""));
+        }        
+    }
+}
